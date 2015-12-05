74bc6819e4d92f5adb8d06537838dae12209771c
Ryan Kuhle <ryan@ryankuhle.com>
2015-12-05 16:48:09 -0600

    Save raw data, less HTML, into  list

--- education.py
+++ education.py
@@ -27,23 +27,15 @@ import requests
 url = "http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm"
 r = requests.get(url)
 soup = BeautifulSoup(r.content)
-table = soup('table')[6] #full table  of content that we want
-table_rows = table.findAll('tr') #,all of the rows that we want
-table_rows = table_rows[8:191] #rows containing the data that we want
+table = soup('table')[6]
 
 data = []
-#table = soup.find('table', attrs={'class':'lineItemsTable'})
-#table_body = table.find('tbody')
 
-rows = table.findAll('tr')[8:191]
-for row in rows:
+#rows = table.findAll('tr')[8:191]
+for row in table.findAll('tr')[8:191]:
     cols = row.findAll('td')
     cols = [ele.text.strip() for ele in cols]
-    data.append([ele for ele in cols if ele]) # Get rid of empty values
+    data.append([ele for ele in cols if ele])
+    #some items have 5 columns, some have 6. Need to eliminate unneeded column
 
 print data
-#for rows in table_rows:
-#    rows = rows.rstrip('</tr>')
-#    print rows
-#    plit by td tag
-#    Insert into database
