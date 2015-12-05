7463971b4f5b3ef02d89370df9b9b64a79484f48
Ryan Kuhle <ryan@ryankuhle.com>
2015-12-05 17:36:49 -0600

    Insert  clean data into  life_expectancy  table

--- education.py
+++ education.py
@@ -4,14 +4,15 @@ import sqlite3 as lite
 
 url = "http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm"
 
+con = lite.connect('education.db')
+
 def createTable():
     '''createTable
     Create SQLite table to store education data in.
     '''
-    con = lite.connect('education.db')
     cur = con.cursor()
     with con:
-        cur.execute('CREATE TABLE life_expectancy (id INT PRIMARY KEY, country TEXT, year INT, male INT, female INT)')
+        cur.execute('CREATE TABLE life_expectancy (country TEXT, year INT, male INT, female INT)')
     print "Table creation successful."
 
 def getCleanInsert(url):
@@ -28,9 +29,12 @@ def getCleanInsert(url):
         country = cols[0]
         year = int(cols[1])
         male = int(cols[7])
-        female = float(cols[10])
-        print "%s, %s, %s, %s" % (country, year, male, female)
+        female = int(cols[10])
+        info = (country, year, male, female)
+        cur = con.cursor()
+        with con:
+            cur.execute('INSERT INTO life_expectancy (country, year, male, female) VALUES (?,?,?,?)', info)
+        print "Inserting data for: %s" % (country)
 
 createTable()
-
 getCleanInsert(url)
