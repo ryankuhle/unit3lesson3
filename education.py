2fa0d5c76fe172ef379888a820da7168fe9809bc
Ryan Kuhle <ryan@ryankuhle.com>
2015-12-05 17:19:31 -0600

    Create life_expectancy table

--- education.py
+++ education.py
@@ -1,7 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
+import sqlite3 as lite
 
 url = "http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm"
+
+def createTable():
+    '''createTable
+    Create SQLite table to store education data in.
+    '''
+    con = lite.connect('education.db')
+    cur = con.cursor()
+    with con:
+        cur.execute('CREATE TABLE life_expectancy (id INT PRIMARY KEY, country TEXT, year INT, male INT, female INT)')
+    print "Table creation successful."
 
 def getCleanInsert(url):
     '''getCleanInsert
@@ -15,10 +26,11 @@ def getCleanInsert(url):
         cols = row.findAll('td')
         cols = [ele.text.strip() for ele in cols]
         country = cols[0]
-        year = cols[1]
-        men = int(cols[7])
-        women = float(cols[10])
-        total = (men + women) / 2
-        print "%s, %s, %s, %s, %s" % (country, year, total, men, women)
+        year = int(cols[1])
+        male = int(cols[7])
+        female = float(cols[10])
+        print "%s, %s, %s, %s" % (country, year, male, female)
+
+createTable()
 
 getCleanInsert(url)
