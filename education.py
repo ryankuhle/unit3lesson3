8306047a3273f67b99e135ab5598d523a9201aa2
Ryan Kuhle <ryan@ryankuhle.com>
2015-12-05 16:44:44 -0600

    Save raw data, less HTML, into  list

--- education.py
+++ education.py
@@ -17,6 +17,8 @@
 <td align="right" height="19">11</td>
 <td height="19"></td>
 </tr>
+
+I want the data in columns: 0, 1, 4, 7, 10
 """
 
 from bs4 import BeautifulSoup
@@ -25,13 +27,23 @@ import requests
 url = "http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm"
 r = requests.get(url)
 soup = BeautifulSoup(r.content)
-table = soup('table')[6]
-table_rows = table.findAll('tr') #, { "class" : "tcont" })
-table_rows = table_rows[8:191]
-print table_rows #Raw data that needs to be split
+table = soup('table')[6] #full table  of content that we want
+table_rows = table.findAll('tr') #,all of the rows that we want
+table_rows = table_rows[8:191] #rows containing the data that we want
 
+data = []
+#table = soup.find('table', attrs={'class':'lineItemsTable'})
+#table_body = table.find('tbody')
 
-#split each row  based on probably td tag
+rows = table.findAll('tr')[8:191]
+for row in rows:
+    cols = row.findAll('td')
+    cols = [ele.text.strip() for ele in cols]
+    data.append([ele for ele in cols if ele]) # Get rid of empty values
 
-
-#insert content into a pandas
+print data
+#for rows in table_rows:
+#    rows = rows.rstrip('</tr>')
+#    print rows
+#    plit by td tag
+#    Insert into database
